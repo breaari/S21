aef17e7b264d8edcb2a4a932740555adc4a8f9d8
abrea.prog <aru.brea@gmail.com>
2024-04-23 02:28:02 -0300

    algo

--- front-S21/src/Components/Programas/Pregrado.jsx
+++ front-S21/src/Components/Programas/Pregrado.jsx
@@ -1,26 +1,35 @@
 export const Pregrado = [
     "Martillero, Corredor Público y Corredor Inmobiliario",
     "Procurador",
-    "Tec. en Investigación de la Escena del Crimen",
+
+    // "Tec. en Investigación de la Escena del Crimen",
+
     "Tec. en Administración y Gestión Tributaria",
     "Tec. en Gestión de Empresas Familiares",
     "Tec. en Administración y Gestión de Políticas Públicas",
     "Tec. en Responsabilidad y Gestión Social",
     "Tec. en Higiene y Seguridad Laboral",
     "Tec. en Diseño y Animación Digital",
-    "Tec. en Marketing y Publicidad Digital",
-    "Tec. en Gestión de Recursos Turísticos",
-    "Tec. en Hidrocarburos y Geociencias",
-    "Tec. en Gestión y Auditorías Ambientales",
+
+    // "Tec. en Marketing y Publicidad Digital",
+    // "Tec. en Gestión de Recursos Turísticos",
+
+   "Tec. en Hidrocarburos y Geociencias",
+
+    // "Tec. en Gestión y Auditorías Ambientales",
+
     "Tec. en Dirección de Equipos de Venta",
-    "Tec. en Gestión Contable e Impositiva",
+
+    // "Tec. en Gestión Contable e Impositiva",
+
     "Tec. en Relaciones Laborales",
     "Tec. en Dir. de Protocolo, Org. de Eventos y RRPP",
     "Tec. en Gestión de Moda",
-    "Tec. en Gestión del Clima Laboral en la Organización",
-    "Tec. en Promoción Comunitaria en Niñez y Adolesc.",
-    "Tec. en Gestión Administrativa de Servicios de Salud",
-    "Tec. en Redes Informáticas y Telecomunicaciones",
-    "Tec. en Adm. de la Prop. Horizontal y Conjuntos Inmob.",
-    "Tec. en Diseño y Desarrollo de Videojuegos"
+
+    // "Tec. en Gestión del Clima Laboral en la Organización",
+    // "Tec. en Promoción Comunitaria en Niñez y Adolesc.",
+    // "Tec. en Gestión Administrativa de Servicios de Salud",
+    // "Tec. en Redes Informáticas y Telecomunicaciones",
+    // "Tec. en Adm. de la Prop. Horizontal y Conjuntos Inmob.",
+    // "Tec. en Diseño y Desarrollo de Videojuegos"
 ]
