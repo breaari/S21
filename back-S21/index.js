7d1c60a3b2d150b041eca08b2630a58fcd6993df
abrea.prog <aru.brea@gmail.com>
2024-04-23 16:56:01 -0300

    probando sin base de datos

--- back-S21/index.js
+++ back-S21/index.js
@@ -1,13 +1,15 @@
-const { conn } = require('./src/DB_conection')
+//const { conn } = require('./src/DB_conection')
 const server = require('./src/app.js');
 const { DB_PORT } = process.env || 3000
 
+// conn.sync({ alter:true }).then(() => {
+//     server.listen(DB_PORT, () => {
+//       console.log(
+//         `Servidor Express en funcionamiento en el puerto ${DB_PORT}`
+//       );
+//     });
+//   });
 
-
-conn.sync({ alter:true }).then(() => {
-    server.listen(DB_PORT, () => {
-      console.log(
-        `Servidor Express en funcionamiento en el puerto ${DB_PORT}`
-      );
-    });
-  });
+server.listen(DB_PORT, () => {
+  console.log(`Servidor Express en funcionamiento en el puerto ${DB_PORT}`);
+});
